1072468dd496ded1de5fa4f7fdfa0bea6cdf56d4
nanba <nanba@NANBA-AEROG11.cac-k.jp.local>
2026-04-16 14:49:30 +0900

    途中

--- java_comprehension_exercises_volume1/src/advanced/question07/Question07_5.java
+++ java_comprehension_exercises_volume1/src/advanced/question07/Question07_5.java
@@ -1,6 +1,8 @@
 package advanced.question07;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Question07_5 {
 
@@ -9,10 +11,19 @@ public class Question07_5 {
 		// 登録商品（値は変更可）
 		String[] itemNames = { "ノート", "ボールペン", "消しゴム", "はさみ", "のり" };
 		// 単価（値は変更可）
-		int[] prices = { 120, 80, 50, 250, 100 };
+		int[] prices = { 120, 200, 100, 400, 150 };
 		// 購入数（変更不可）
 		int[] amounts = new int[5];
 
 		// TODO: 実装ここから
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+		System.out.println("商品名：単価〇円（税込〇円）× 〇個 = 〇円");
+		System.out.println();
+
+		System.out.println("購入内容一覧：");
+		for (int i = 0; i < amounts.length; i++) {
+			System.out.print(itemNames[i] + "：単価" + prices[i] + " 円 （税込" + (int) (prices[i] * 1.1) + " 円）");
+		}
 	}
 }
