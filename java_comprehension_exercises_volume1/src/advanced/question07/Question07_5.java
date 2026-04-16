8d5cf040c6e91a03c871e1c27ba98439ad60b5a3
nanba <nanba@NANBA-AEROG11.cac-k.jp.local>
2026-04-16 15:20:54 +0900

    question07_5 修正

--- java_comprehension_exercises_volume1/src/advanced/question07/Question07_5.java
+++ java_comprehension_exercises_volume1/src/advanced/question07/Question07_5.java
@@ -22,14 +22,14 @@ public class Question07_5 {
 		for (int i = 0; i < amounts.length; i++) {
 			System.out.print(itemNames[i] + "の購入数:>");
 			amounts[i] = Integer.parseInt(reader.readLine());
-			sum += prices[i] * amounts[i];
+			sum += (int) (prices[i] * 1.1) * amounts[i];
 		}
 		System.out.println();
 		System.out.println("購入内容一覧：");
 		for (int i = 0; i < amounts.length; i++) {
 			System.out.println(itemNames[i] + "：単価" + prices[i] + " 円 （税込" + (int) (prices[i] * 1.1) + " 円） × "
-					+ amounts[i] + "個 = " + (int) (prices[i] * 1.1 * amounts[i]) + " 円");
-			sum += (int) (prices[i] * 1.1 * amounts[i]);
+					+ amounts[i] + "個 = " + (int) (prices[i] * 1.1) * amounts[i] + " 円");
+			sum += (int) (prices[i] * 1.1) * amounts[i];
 		}
 		System.out.println("合計（税込）：" + sum + " 円");
 		System.out.println("割引後合計（税込）：" + (int) (sum * .9) + " 円");
